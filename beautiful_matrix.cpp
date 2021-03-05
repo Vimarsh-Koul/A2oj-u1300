c869f6a57edfbd931617116ff27000ec97d133d7
Vimarsh-Koul <vimarsh2001koul@gmail.com>
2021-03-05 22:13:10 +0530

    Mar 5

---
+++ beautiful_matrix.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int a[5][5];
+	for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++)
+			cin>>a[i][j];
+	}
+	int x,y;
+
+	for(int i=0;i<5;i++){
+		for(int j=0;j<5;j++)
+			if(a[i][j]==1){
+				x=i;
+				y=j;
+				break;
+			}
+	}
+
+
+	int steps = abs(x-2) + abs(y-2);
+	cout<<steps<<endl;
+}
